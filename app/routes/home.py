01e167226e838eece1e3bcb3ed4cb48a8e23163c
Deisel <74831846+Amelia-was@users.noreply.github.com>
2021-07-07 09:13:42 -0400

    posts populate homepage

--- app/routes/home.py
+++ app/routes/home.py
@@ -1,14 +1,19 @@
 from flask import Blueprint, render_template
+from app.models import User, Post
+from app.db import get_db
 
 bp = Blueprint('home', __name__, url_prefix='/')
 
 @bp.route('/')
 def index():
     # get all posts
-    #
-    return (render_template(
-        'homepage.html'
-    ))
+    db = get_db()
+    posts = db.query(Post).order_by(Post.created_at.desc()).all()
+
+    return render_template(
+        'homepage.html',
+        posts=posts
+    )
 
 @bp.route('/login')
 def login():
@@ -17,15 +22,20 @@ def login():
 @bp.route('/post/<id>')
 def single(id):
     # get single post by id
-    #
+    db = get_db()
+    post = db.query(Post).filter(Post.id == id).one()
+
     return render_template(
-        'single-post.html'
+        'single-post.html',
+        post=post
     )
 
 @bp.route('/user/<id>')
 def single_user(id):
     # get single user by id
-    #
+    db = get_db()
+    post = db.query(User).filter(Post.id == id).one()
+
     return render_template(
         'single-user.html'
     )
